d12938dcc67d3a7d175bfc0c95a7f7e19bc51d06
robotganteng09 <arsyafaisyar@gmail.com>
2025-09-29 19:03:54 +0700

    add product fix

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use("/uploads", express.static("uploads"));
+app.use(express.urlencoded({ extended: true })); 
 app.use("/api/auth", authRoutes);
 app.use("/api/products",productRoutes); 
 
