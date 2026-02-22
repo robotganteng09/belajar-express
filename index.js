ff5bc5027c0cb0da3425ecd4c9ed2624b1b029d1
robotganteng09 <arsyafaisyar@gmail.com>
2026-02-22 10:57:37 +0700

    pasang nodemon

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(express.json());
 
-// Routes
-const authRoutes = require("./routes/authRoutes");
-app.use("/api", authRoutes);
-
-// Server
 app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
+  console.log(`Server jalan di http://localhost:${PORT}`);
 });
